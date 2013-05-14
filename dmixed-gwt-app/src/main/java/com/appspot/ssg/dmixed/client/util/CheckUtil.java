f86d2e407f5911439d35761106fc4c4551191007
ProProMarco <propromarco@web.de>
2013-05-14 11:14:29 +0000

    Fix

---
+++ dmixed-gwt-app/src/main/java/com/appspot/ssg/dmixed/client/util/CheckUtil.java
@@ -0,0 +1,98 @@
+package com.appspot.ssg.dmixed.client.util;
+
+import com.appspot.ssg.dmixed.client.activities.TerminActivity.TerminView.IListItem;
+import com.appspot.ssg.dmixed.client.model.MitbringData;
+import com.appspot.ssg.dmixed.client.model.TeilnahmeData;
+import com.appspot.ssg.dmixed.client.model.TerminTeilnehmer;
+import com.appspot.ssg.dmixed.shared.ETeilnahmeStatus;
+import com.appspot.ssg.dmixed.shared.IAsync;
+import com.appspot.ssg.dmixed.shared.IDMixedUsecase;
+import com.appspot.ssg.dmixed.shared.ITerminMitbringsel;
+import com.appspot.ssg.dmixed.shared.ITerminTeilnehmer;
+
+public class CheckUtil {
+    public static boolean checkTeilnehmer(final IDMixedUsecase service, final Long userId, final ITerminTeilnehmer terminTeilnehmer,
+	    final Long terminId, final ETeilnahmeStatus status) {
+	if (!terminTeilnehmer.isChangeAllowed())
+	    return false;
+	terminTeilnehmer.setTeilnahme(status);
+	final TeilnahmeData teilnahmeData = new TeilnahmeData();
+	teilnahmeData.setId(userId);
+	teilnahmeData.setTerminId(terminId);
+	teilnahmeData.setTeilnahme(status);
+	final IAsync<Void> answer = new IAsync<Void>() {
+	    @Override
+	    public void onSuccess(final Void t) {
+	    }
+
+	    @Override
+	    public void onError(final Throwable exception) {
+		exception.printStackTrace();
+	    }
+	};
+	service.onTeilnahme(teilnahmeData, answer);
+	// Fire newValue to server
+	return true;
+    }
+
+    public static boolean checkMitbringsel(final IDMixedUsecase service, final Long userId, final Long terminId, final IListItem<Boolean> item,
+	    final ITerminMitbringsel terminMitbringsel) {
+	final ITerminTeilnehmer mitbringer = terminMitbringsel.getMitbringer();
+	if (mitbringer == null) {
+	    // Es bringt noch keiner mit
+	    final TerminTeilnehmer ich = new TerminTeilnehmer();
+	    ich.setId(userId);
+	    // ich.setVorname(user.getVorname());
+	    // ich.setName(user.getName());
+	    terminMitbringsel.setMitbringer(ich);
+	    final MitbringData mitbringData = new MitbringData();
+	    mitbringData.setMitbringselId(terminMitbringsel.getId());
+	    mitbringData.setTerminId(terminId);
+	    mitbringData.setId(userId);
+	    mitbringData.setMitbringen(true);
+	    final IAsync<Void> answer = new IAsync<Void>() {
+		@Override
+		public void onSuccess(final Void t) {
+		    item.setMitbringer("*");
+		}
+
+		@Override
+		public void onError(final Throwable exception) {
+		    exception.printStackTrace();
+		}
+	    };
+	    service.onMitbringen(mitbringData, answer);
+	    // Fire ich brings mit
+	    return true;
+	} else {
+	    // Es bringt schon einer mit
+	    final Long id = mitbringer.getId();
+	    if (id.equals(userId)) {
+		// Ich war eingetragen
+		terminMitbringsel.setMitbringer(null);
+		final MitbringData mitbringData = new MitbringData();
+		mitbringData.setMitbringselId(terminMitbringsel.getId());
+		mitbringData.setTerminId(terminId);
+		mitbringData.setId(userId);
+		mitbringData.setMitbringen(false);
+		final IAsync<Void> answer = new IAsync<Void>() {
+		    @Override
+		    public void onSuccess(final Void t) {
+			item.setMitbringer(null);
+		    }
+
+		    @Override
+		    public void onError(final Throwable exception) {
+			exception.printStackTrace();
+		    }
+		};
+		service.onMitbringen(mitbringData, answer);
+		// Fire ich brings nicht mehr mit
+		return false;
+	    } else {
+		// Ich darf das nicht
+		return true;
+	    }
+	}
+    }
+}
--- dmixed-gwt-app/src/main/java/com/appspot/ssg/dmixed/client/views/TerminViewImpl.java
+++ dmixed-gwt-app/src/main/java/com/appspot/ssg/dmixed/client/views/TerminViewImpl.java
@@ -2,20 +2,16 @@ package com.appspot.ssg.dmixed.client.views;
 
 import com.appspot.ssg.dmixed.client.IDMixedMessages;
 import com.appspot.ssg.dmixed.client.activities.TerminActivity.TerminView;
+import com.appspot.ssg.dmixed.client.views.components.MitbringRadioButton;
+import com.appspot.ssg.dmixed.client.views.components.TeilnehmerListBox;
 import com.appspot.ssg.dmixed.shared.ETeilnahmeStatus;
 import com.appspot.ssg.dmixed.shared.ITerminMitbringsel;
 import com.appspot.ssg.dmixed.shared.ITerminTeilnehmer;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.googlecode.mgwt.dom.client.event.tap.TapHandler;
 import com.googlecode.mgwt.ui.client.widget.FormListEntry;
-import com.googlecode.mgwt.ui.client.widget.MListBox;
-import com.googlecode.mgwt.ui.client.widget.MRadioButton;
 import com.googlecode.mgwt.ui.client.widget.ScrollPanel;
 import com.googlecode.mgwt.ui.client.widget.WidgetList;
 import com.googlecode.mgwt.ui.client.widget.tabbar.BookmarkTabBarButton;
@@ -23,102 +19,16 @@ import com.googlecode.mgwt.ui.client.widget.tabbar.DownloadsTabBarButton;
 import com.googlecode.mgwt.ui.client.widget.tabbar.MostViewedTabBarButton;
 import com.googlecode.mgwt.ui.client.widget.tabbar.TabPanel;
 
-public class TerminViewImpl extends AbstractDmixedView implements TerminView {
-
-    public class InternalListBox extends MListBox implements IListItem<ETeilnahmeStatus> {
-
-	public InternalListBox() {
-	    final ETeilnahmeStatus[] values = ETeilnahmeStatus.values();
-	    for (final ETeilnahmeStatus teilnahmeStatus : values) {
-		addItem(getListText(teilnahmeStatus), teilnahmeStatus.name());
-	    }
-	}
-
-	private String getListText(final ETeilnahmeStatus status) {
-	    switch (status) {
-	    case NimmtTeil:
-		return _messages.nimmtTeil();
-	    case NimmtNichtTeil:
-		return _messages.nimmtNichtTeil();
-	    case Vielleicht:
-		return _messages.vielleicht();
-	    case NichtEntschieden:
-	    default:
-		return _messages.nichtEntschieden();
-	    }
-	}
-
-	@Override
-	public ETeilnahmeStatus getValue() {
-	    final int selectedIndex = getSelectedIndex();
-	    final String value = getValue(selectedIndex);
-	    return ETeilnahmeStatus.valueOf(value);
-	}
-
-	@Override
-	public void setValue(final ETeilnahmeStatus value) {
-	    final int index = value.ordinal();
-	    setSelectedIndex(index);
-	}
-
-	@Override
-	public void setValue(final ETeilnahmeStatus value, final boolean fireEvents) {
-	    // TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public HandlerRegistration addValueChangeHandler(final ValueChangeHandler<ETeilnahmeStatus> handler) {
-	    return addChangeHandler(new ChangeHandler() {
-
-		@Override
-		public void onChange(final ChangeEvent event) {
-		    final ValueChangeEvent<ETeilnahmeStatus> evt = new ValueChangeEvent<ETeilnahmeStatus>(getValue()) {
-		    };
-		    handler.onValueChange(evt);
-		}
-	    });
-	}
-
-	@Override
-	public HandlerRegistration addTapHandler(final TapHandler handler) {
-	    // TODO Auto-generated method stub
-	    return null;
-	}
-
-	@Override
-	public void setMitbringer(final String string) {
-	}
-
-    }
-
-    public class InternalRadioButton extends MRadioButton implements IListItem<Boolean> {
-
-	private final String _text;
-
-	public InternalRadioButton(final String name) {
-	    super(name);
-	    _text = name;
-	}
-
-	@Override
-	public void setMitbringer(final String user) {
-	    if (user != null) {
-		setText(_text + "(" + user + ")");
-	    } else {
-		setText(_text);
-	    }
-	}
-
-    }
+public class TerminViewImpl extends AbstractDmixedView implements TerminView, SelectionHandler<Integer> {
 
     private final HTML _beschreibung;
-    private final WidgetList _mitbrinselList;
-    private final WidgetList _teilnehmerList;
+    private final WidgetList _teilnehmerList, _mitbrinselList;
+    private ScrollPanel teilnehmerScrollPanel, mitbringselScrollPanel;
 
     public TerminViewImpl(final IDMixedMessages messages) {
 	super(messages);
 	final TabPanel tabPanel = new TabPanel();
+	tabPanel.addSelectionHandler(this);
 	{
 	    // Page 1
 	    final FlowPanel container = new FlowPanel();
@@ -131,29 +41,25 @@ public class TerminViewImpl extends AbstractDmixedView implements TerminView {
 	}
 	{
 	    // Page 2
-	    final FlowPanel container = new FlowPanel();
 	    _teilnehmerList = new WidgetList();
-	    final ScrollPanel teilnehmerScrollPanel = new ScrollPanel();
+	    this.teilnehmerScrollPanel = new ScrollPanel();
 	    teilnehmerScrollPanel.setWidget(_teilnehmerList);
 	    teilnehmerScrollPanel.setScrollingEnabledX(false);
-	    container.add(teilnehmerScrollPanel);
 	    final String mitkommen = messages.mitkommen();
 	    final MostViewedTabBarButton teilnehmer = new MostViewedTabBarButton();
 	    teilnehmer.setText(mitkommen);
-	    tabPanel.add(teilnehmer, container);
+	    tabPanel.add(teilnehmer, teilnehmerScrollPanel);
 	}
 	{
 	    // Page 3
-	    final FlowPanel container = new FlowPanel();
 	    _mitbrinselList = new WidgetList();
-	    final ScrollPanel mitbringselScrollPanel = new ScrollPanel();
+	    this.mitbringselScrollPanel = new ScrollPanel();
 	    mitbringselScrollPanel.setWidget(_mitbrinselList);
 	    mitbringselScrollPanel.setScrollingEnabledX(false);
-	    container.add(mitbringselScrollPanel);
 	    final String mitbringen = messages.mitbringen();
 	    final DownloadsTabBarButton teilnehmer = new DownloadsTabBarButton();
 	    teilnehmer.setText(mitbringen);
-	    tabPanel.add(teilnehmer, container);
+	    tabPanel.add(teilnehmer, mitbringselScrollPanel);
 	}
 	_layoutPanel.add(tabPanel);
 
@@ -169,20 +75,16 @@ public class TerminViewImpl extends AbstractDmixedView implements TerminView {
 	_teilnehmerList.clear();
 	return new IListCreator<ITerminTeilnehmer, ETeilnahmeStatus>() {
 	    @Override
-	    public IListItem<ETeilnahmeStatus> create(final ITerminTeilnehmer terminTeilnehmer) {
+	    public IListItem<ETeilnahmeStatus> create(final ITerminTeilnehmer terminTeilnehmer, final boolean loggedUser) {
 		final String vorname = terminTeilnehmer.getVorname();
 		final String name = terminTeilnehmer.getName();
 		final String komplett = vorname + " " + name;
 		final ETeilnahmeStatus teilnahme = terminTeilnehmer.getTeilnahme();
-		final InternalListBox androidRadioButton = new InternalListBox();
+		final TeilnehmerListBox androidRadioButton = new TeilnehmerListBox(_messages, loggedUser);
 		androidRadioButton.setValue(teilnahme);
 		final FormListEntry entry = new FormListEntry(komplett, androidRadioButton);
 		_teilnehmerList.add(entry);
 		return androidRadioButton;
-	    }
-
-	    @Override
-	    public void onFinish(final boolean hasItems) {
 	    }
 	};
     }
@@ -192,10 +94,10 @@ public class TerminViewImpl extends AbstractDmixedView implements TerminView {
 	_mitbrinselList.clear();
 	return new IListCreator<ITerminMitbringsel, Boolean>() {
 	    @Override
-	    public IListItem<Boolean> create(final ITerminMitbringsel terminMitbringsel) {
+	    public IListItem<Boolean> create(final ITerminMitbringsel terminMitbringsel, final boolean loggedUser) {
 		final String beschreibung = terminMitbringsel.getBeschreibung();
 		final ITerminTeilnehmer mitbringer = terminMitbringsel.getMitbringer();
-		final InternalRadioButton androidRadioButton = new InternalRadioButton(beschreibung);
+		final MitbringRadioButton androidRadioButton = new MitbringRadioButton(beschreibung);
 		androidRadioButton.setText(beschreibung);
 		final boolean hatMitbringer = mitbringer != null;
 		androidRadioButton.setValue(hatMitbringer);
@@ -204,10 +106,12 @@ public class TerminViewImpl extends AbstractDmixedView implements TerminView {
 		_mitbrinselList.add(androidRadioButton);
 		return androidRadioButton;
 	    }
-
-	    @Override
-	    public void onFinish(final boolean hasItems) {
-	    }
 	};
+    }
+
+    @Override
+    public void onSelection(final SelectionEvent<Integer> event) {
+	teilnehmerScrollPanel.refresh();
+	mitbringselScrollPanel.refresh();
     }
 }
