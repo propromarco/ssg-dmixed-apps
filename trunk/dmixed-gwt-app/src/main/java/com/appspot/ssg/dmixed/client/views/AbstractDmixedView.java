8cbb9e88e04e878116c87087aaafc316f99f19d4
thetonie@googlemail.com <thetonie@googlemail.com@52079bb0-d945-c3e3-7d45-3562a34e2657>
2013-05-27 12:11:26 +0000

    kleine Änderungen.. wir müssen aber mal über Struktur reden ;)

--- trunk/dmixed-gwt-app/src/main/java/com/appspot/ssg/dmixed/client/views/AbstractDmixedView.java
+++ trunk/dmixed-gwt-app/src/main/java/com/appspot/ssg/dmixed/client/views/AbstractDmixedView.java
@@ -1,3 +1,5 @@
+/* Copyright, (c) 2013 Suretec GmbH  */
+
 package com.appspot.ssg.dmixed.client.views;
 
 import com.appspot.ssg.dmixed.client.IDMixedCss;
@@ -5,13 +7,14 @@ import com.appspot.ssg.dmixed.client.IDMixedMessages;
 import com.appspot.ssg.dmixed.client.activities.IDMixedView;
 import com.appspot.ssg.dmixed.client.views.components.Header;
 import com.appspot.ssg.dmixed.client.views.components.LayoutPanel;
+
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 
-public abstract class AbstractDmixedView extends Composite implements IDMixedView {
-
+public abstract class AbstractDmixedView extends Composite implements IDMixedView
+{
     protected final IDMixedMessages _messages;
     protected final IDMixedCss css;
     protected final FlowPanel _layoutPanel;
@@ -19,40 +22,41 @@ public abstract class AbstractDmixedView extends Composite implements IDMixedVie
 
     // private final ProgressBar _progressBar;
 
-    public AbstractDmixedView(final IDMixedMessages messages, final IDMixedCss css) {
-	_messages = messages;
-	this.css = css;
-	_layoutPanel = new LayoutPanel(css);
-	initWidget(_layoutPanel);
-	_headerPanel = new Header(css);
-	_layoutPanel.add(_headerPanel);
-	// _progressBar = new ProgressBar();
-	// _progressBar.setVisible(false);
-	// _headerPanel.setRightWidget(_progressBar);
+    public AbstractDmixedView( final IDMixedMessages messages, final IDMixedCss css )
+    {
+        _messages = messages;
+        this.css = css;
+        _layoutPanel = new LayoutPanel(css);
+        initWidget(_layoutPanel);
+        _headerPanel = new Header(css);
+        _layoutPanel.add(_headerPanel);
+        // _progressBar = new ProgressBar();
+        // _progressBar.setVisible(false);
+        // _headerPanel.setRightWidget(_progressBar);
     }
 
-    @Override
-    public void setHeaderText(final String headerText) {
-	_headerPanel.setHTML(headerText);
+    @Override public void setHeaderText( final String headerText )
+    {
+        _headerPanel.setHTML(headerText);
+        _headerPanel.setStyleName(css.form_header_label());
     }
 
-    @Override
-    public void setProgress(final boolean visible) {
-	// _progressBar.setVisible(visible);
+    @Override public void setProgress( final boolean visible )
+    {
+        // _progressBar.setVisible(visible);
     }
 
-    @Override
-    public void showMessage(final String message) {
-	final DialogBox dialogBox = new DialogBox(true, true);
-	dialogBox.add(new HTML(message));
-	dialogBox.center();
+    @Override public void showMessage( final String message )
+    {
+        final DialogBox dialogBox = new DialogBox(true, true);
+        dialogBox.add(new HTML(message));
+        dialogBox.center();
     }
 
-    @Override
-    public void showError(final Throwable exception) {
-	final DialogBox dialogBox = new DialogBox(true, true);
-	dialogBox.add(new HTML(exception.getMessage()));
-	dialogBox.center();
+    @Override public void showError( final Throwable exception )
+    {
+        final DialogBox dialogBox = new DialogBox(true, true);
+        dialogBox.add(new HTML(exception.getMessage()));
+        dialogBox.center();
     }
-
 }
